84bba5d4816d053cf7184c59569e27e4b0df6078
Kunal007777777 <105564185+Kunal007777777@users.noreply.github.com>
2026-04-20 22:23:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,9 +120,9 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/technokrrish') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/technokrrishyt') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/ip_update') # Update channel link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+F3zHFMEAYC5lYmY1') # Group link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Extreme_Developer') # Owner link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/Team_X_Crypto_Bitcoin_News_Chann') # Update channel link for the bot
 
 # ============================
 # User Configuration
